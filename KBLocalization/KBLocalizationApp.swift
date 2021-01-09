5365c5be04c6f36adc211f9e892b4049b447ab7e
g.kolosov <90kolosov@gmail.com>
2021-01-09 11:28:10 +0300

    Initial Commit

---
+++ KBLocalization/KBLocalizationApp.swift
@@ -0,0 +1,17 @@
+//
+//  KBLocalizationApp.swift
+//  KBLocalization
+//
+//  Created by Gregory Kolosov on 09.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct KBLocalizationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
